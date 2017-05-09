7dde2325a9fb6b284a53dbe21db3a9c3dae9ce40
mssq <smiikasiren@gmail.com>
2017-05-09 17:39:54 +0300

    Modified redirect after succesful registeration

--- Mehupojat/src/main/java/fi/c5msiren/web/UserController.java
+++ Mehupojat/src/main/java/fi/c5msiren/web/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
 
         securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
 
-        return "redirect:/welcome";
+        return "redirect:/index.html";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
