7287df97a42678c5f55f8e6c2c504b3e6f77f342
mssq <smiikasiren@gmail.com>
2017-05-10 14:29:43 +0300

    Added Javadoc

--- Mehupojat/src/main/java/fi/c5msiren/service/UserService.java
+++ Mehupojat/src/main/java/fi/c5msiren/service/UserService.java
@@ -2,8 +2,27 @@ package fi.c5msiren.service;
 
 import fi.c5msiren.model.User;
 
+/**
+ * Interface for user service
+ *
+ * @author Miika
+ * @version 2017.5.09
+ * @since 1.8
+ */
 public interface UserService {
+
+    /**
+     * Method for saving a user
+     *
+     * @param user User to be saved
+     */
     void save(User user);
 
+    /**
+     * Method for finding user by username
+     *
+     * @param username Username of the user
+     * @return the user with the username
+     */
     User findByUsername(String username);
 }
