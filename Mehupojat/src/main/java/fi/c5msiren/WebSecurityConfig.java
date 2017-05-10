44f51c3f5a6a073ec321d47e2811e55f45a82977
mssq <smiikasiren@gmail.com>
2017-05-10 15:07:12 +0300

    Added Javadoc

--- Mehupojat/src/main/java/fi/c5msiren/WebSecurityConfig.java
+++ Mehupojat/src/main/java/fi/c5msiren/WebSecurityConfig.java
@@ -9,12 +9,28 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+/**
+ * This class acts as a configuration of web security
+ *
+ * @author Miika
+ * @version 2017.5.09
+ * @since 1.8
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Reference to UserDetailsService class
+     */
     @Autowired
     private UserDetailsService userDetailsService;
 
+    /**
+     * Method for configuring the security
+     * 
+     * @param http Http security object
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -33,6 +49,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll();
     }
 
+    /**
+     * Method for configuring the security globally
+     * 
+     * @param auth Builder for authentication manager
+     */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
