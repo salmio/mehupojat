e9cbf0d3d5c66446c80bb7bbec722cee9555b9cd
mssq <smiikasiren@gmail.com>
2017-05-10 14:10:12 +0300

    Added Javadoc

--- Mehupojat/src/main/java/fi/c5msiren/repository/RoleRepository.java
+++ Mehupojat/src/main/java/fi/c5msiren/repository/RoleRepository.java
@@ -3,5 +3,12 @@ package fi.c5msiren.repository;
 import fi.c5msiren.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interface for roles repository
+ *
+ * @author Miika
+ * @version 2017.5.09
+ * @since 1.8
+ */
 public interface RoleRepository extends JpaRepository<Role, Long>{
 }
