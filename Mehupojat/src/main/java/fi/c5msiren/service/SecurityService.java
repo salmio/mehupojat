71629f77d87ef13146c31bcc5c5d538decde550a
mssq <smiikasiren@gmail.com>
2017-05-10 14:27:25 +0300

    Added Javadoc

--- Mehupojat/src/main/java/fi/c5msiren/service/SecurityService.java
+++ Mehupojat/src/main/java/fi/c5msiren/service/SecurityService.java
@@ -1,7 +1,26 @@
 package fi.c5msiren.service;
 
+/**
+ * Interface for security service
+ *
+ * @author Miika
+ * @version 2017.5.09
+ * @since 1.8
+ */
 public interface SecurityService {
+
+    /**
+     * Method for finding logged in username
+     *
+     * @return Logged in username
+     */
     String findLoggedInUsername();
 
+    /**
+     * Method for logging in automatically
+     *
+     * @param username Username of the user
+     * @param password Password of the user
+     */
     void autologin(String username, String password);
 }
