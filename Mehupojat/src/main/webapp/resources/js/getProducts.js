276dccd00a5ca7c9d5d987b846543ebeca8bb9a7
mssq <smiikasiren@gmail.com>
2017-05-10 22:34:13 +0300

    Added add to cart button to items which stock is over zero.

--- Mehupojat/src/main/webapp/resources/js/getProducts.js
+++ Mehupojat/src/main/webapp/resources/js/getProducts.js
@@ -60,18 +60,35 @@ var Result = React.createClass({
         );
     }
 });
+
 var ResultItem = React.createClass({
     render:function(){
         var camper = this.props.user;
+        var productStockStatus;
+
+        switch (camper.stock) {
+            case 0:
+                productStockStatus = ""
+                break;
+            default:
+                productStockStatus = 
+                                    <button className="btn btn-lg btn-primary btn-block" type="submit">
+                                    Lisää ostoskoriin</button>
+
+        }
+
         return(
             <div className="col-xs-6 col-sm-4 col-md-3">
                 <div className="product">
                     <div className="col-xs-12"><h3>{camper.name}</h3></div>
                     <div className="col-xs-12"><img src={camper.imageURL} /></div>
-                    <div className="col-xs-12"><h4 id="otsikko">Hinta:&nbsp;</h4> <p>{camper.price}</p></div>
+                    <div className="col-xs-12"><h4 id="otsikko">Hinta:&nbsp;</h4> <p>{camper.price} €</p></div>
                     <div className="col-xs-12"><h4 id="otsikko">Kuvaus:&nbsp;</h4> <p>{camper.description}</p></div>
-                    <div className="col-xs-12"><h4 id="otsikko">Brand:&nbsp;</h4> <p>{camper.brand.name}</p></div>
-                    {/*<div className="col-xs-12"><h4 id="otsikko">Kategori&nbsp;</h4> <p>{camper.categories}</p></div>*/}
+                    <div className="col-xs-12"><h4 id="otsikko">Brändi:&nbsp;</h4> <p>{camper.brand.name}</p></div>
+                    <div className="col-xs-12"><h4 id="otsikko">Varastossa:&nbsp;</h4> <p>{camper.stock}</p></div>
+                    <form method="GET" action={"products/" + camper.id + "/decrease"}>
+                        <div className="col-xs-12">{productStockStatus}</div>
+                    </form>
                 </div>
             </div>
         );
